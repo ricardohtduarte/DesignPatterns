ea615eaf8d0c290feb4dec2eacf8bb444c3255d4
ricardohtduarte <ricardo.duarte@mindera.com>
2020-06-03 17:01:58 +0100

    Behavioral patterns

---
+++ Behavioral/Memento/Memento/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memento
+//
+//  Created by Ricardo Duarte on 03/06/2020.
+//  Copyright © 2020 Ricardo Duarte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
