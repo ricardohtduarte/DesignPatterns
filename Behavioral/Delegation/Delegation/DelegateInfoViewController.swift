ab9b7dffefbe8a27ea6076a022e85c16cacc104a
ricardohtduarte <ricardo.duarte@mindera.com>
2020-06-15 12:56:19 +0100

    Start delegation Pattern

---
+++ Behavioral/Delegation/Delegation/DelegateInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DelegateInfoViewController.swift
+//  Delegation
+//
+//  Created by Ricardo Duarte on 15/06/2020.
+//  Copyright © 2020 Ricardo Duarte. All rights reserved.
+//
+
+import UIKit
+
+class DelegateInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
